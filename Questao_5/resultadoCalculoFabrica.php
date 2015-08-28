4412ba8b32ec440ce270e4cb6bc9b3fe48266632
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-08-28 13:07:42 -0300

    Finalização do exercício 4 e inicio do exercício 5

---
+++ Questao_5/resultadoCalculoFabrica.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Resultado do custo do veiculos</title>
+	</head>
+	<body>
+		<?php 
+			$custo  = $_GET['custoFabrica'];
+ 			$distribuidor = 0.28;
+			$impostos = 0.45;
+			$custoTotal = (($custo*$impostos)+$custo)+(($custo*$distribuidor)+$custo);
+		
+			echo '<font color="blue"><h1 align="center">O custo do veiculo é ' . number_format($custoTotal,2,',','.') . '</h1></font>';
+		?>
+		
+		<form action="fabricaDeCarros.php">
+			<a href="fabricaDeCarros.php">Clique aqui para voltar</a>
+		
+		</form>
+				
+	</body>
+
+
+</html>
