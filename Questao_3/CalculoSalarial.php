e1e5b8fa3e532c828077b5cd2bc6c6b36995da86
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-08-27 21:49:37 -0300

    Finalizado questão 3

--- Questao_3/CalculoSalarial.php
+++ Questao_3/CalculoSalarial.php
@@ -12,10 +12,14 @@
 		}
 	?>
 	<h1 align="center">Calculo Salarial</h1>
-	<p> Digite o valor do seu sálario atual e o percentual de aumento
+	<?php 
+	if (empty($_POST)){
+		echo '<p> Digite o valor do seu sálario atual e o percentual de aumento';
+	}
+	?>
 	<form action="CalculoSalarial.php" method="post">
-		<label for="salarioAtual">Salário atual: </label><input type="number" min="0" required="true" step="any" id="salarioAtual" name="salarioAtual"> <br /> 
-		<label for="percentualAumento">Aumento em percentual: </label><input type="number" min="0" required="true" id="percentualAumento" name="percentualAumento"><br />
+		<label for="salarioAtual">Salário atual: <br /></label><input type="number"  min="0" required="true" step="any" id="salarioAtual" name="salarioAtual"> <br /> 
+		<label for="percentualAumento">Aumento em percentual: <br/></label><input type="number" min="0" required="true" step="any" id="percentualAumento" name="percentualAumento"><br />
 		<input type="submit" value="Enviar"> 
 		<input type="reset" value="Limpar">
 					
@@ -23,7 +27,8 @@
 	<?php
 	if (!empty($_POST)){
 		$resultado = ((($salarioAtual/100)*$percentualAumento)+$salarioAtual);
-		echo '<font color="blue"><h1>O seu novo salário é :' . $resultado . '</h1></font>';
+		echo '<font color="blue"><h3>Com o aumento de ' . $percentualAumento . '%<br />
+					O seu novo salário passa a ser :R$ ' . number_format(($resultado),2,",",".") . '</h3></font>';
 		
 		
 		
