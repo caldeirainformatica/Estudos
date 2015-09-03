aea5e23677883a36c369c854677709112f5fb615
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-09-03 10:52:58 -0300

    Finalizando a 16

--- Questao_15/resultado.php
+++ Questao_15/resultado.php
@@ -9,7 +9,12 @@
 	$hora = intval($segundos / 3600);
 	$minutos = (-1 * (intval(60 * ($hora - ($segundos / 3600)))));
 	$aposVirgula = (60 * ($hora - ($segundos / 3600)));
-	$segundosApurados = 60 * intval(intval(100 * (intval($aposVirgula) - $aposVirgula)) / 60);
+	$segundosApurados = intval(intval(100 * (intval($aposVirgula) - $aposVirgula)) / 60);
+		if ($aposVirgula > 60){
+			$segundosApurados = 60 * $segundosApurados;
+		}
+		echo $aposVirgula;
+		echo $segundosApurados;
 	
-	echo '<h1>O evento levou ' . $hora . 'hs ' . $minutos . ' Min ' . $segundosApurados . ' <br /> </h1>';
+	echo '<h1>O evento levou ' . $hora . 'hs ' . $minutos . ' Min ' . $segundosApurados . ' segundos <br /> </h1>';
 ?>
