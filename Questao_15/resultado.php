e1414645fe9c6f813c3769d9a692acd684b108bc
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-09-03 09:47:09 -0300

    Questão 16

--- Questao_15/resultado.php
+++ Questao_15/resultado.php
@@ -6,9 +6,10 @@
 <?php
 	$segundos = $_POST['segundos'];
 
-	$hora = $segundos / 3600;
-	$minutos = $segundos / 60;
+	$hora = intval($segundos / 3600);
+	$minutos = (-1 * (intval(60 * ($hora - ($segundos / 3600)))));
+	$aposVirgula = (60 * ($hora - ($segundos / 3600)));
+	$segundosApurados = 60 * intval(intval(100 * (intval($aposVirgula) - $aposVirgula)) / 60);
 	
-	echo 'O evento levou ' . $hora . ' horas <br />';
-	echo 'O mesmo evento levou ' . $minutos . ' minutos';
+	echo '<h1>O evento levou ' . $hora . 'hs ' . $minutos . ' Min ' . $segundosApurados . ' <br /> </h1>';
 ?>
