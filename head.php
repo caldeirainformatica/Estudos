1dd8e360343fe28a3da207a53098e4fe24746f99
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-09-13 11:46:19 -0300

    inclusão de bootstrap, javascript e css

---
+++ head.php
@@ -0,0 +1,24 @@
+	<head>
+		<meta charset="utf-8">
+		<title>RICARDO CALDEIRA 14214290044</title>
+	    
+	    <link type="text/css" rel="stylesheet" href="js/bootstrap/css/bootstrap.min.css" />
+	    
+	    <script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+	    <script type="text/javascript" src="js/jquery.maskedinput.min.js"></script>
+	    <script type="text/javascript" src="js/bootstrap/js/bootstrap.min.js"></script>
+	    
+	    
+	    <script type="text/javascript">
+	    	
+	    	$(function(){
+	    		$('.data').mask('99/99/9999');
+	    		$('#cpf').mask('999.999.999-99');
+	    		$('#cep').mask('99.999-999');
+	    		$('#cnpj').mask('99.999.999/9999-99');
+	    		$('#telefone').mask('(99) 9999-9999');
+	    		
+	    	});
+	    
+	    </script>
+    </head>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include_once 'head.php';
+	
+
+	 include_once 'menu.php';
+	 ?>
+	 <html>
+	 	<div class="container" style="margin-top: 60px;">
+	 		 <div class="alert alert-success" role="alert">
+        <strong>BEM VINDO <br/></strong> Utilize os menus acima para navegar pelos exercícios.
+      </div>
+	 	
+	 	</div>
+	 
+	 
+	 </html>
