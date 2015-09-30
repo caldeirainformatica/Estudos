b6b09adeaec07c6d1add9c6c10b332d4e4a5eb1b
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-09-30 18:03:24 -0300

    acabando com conflitos

---
+++ lista5/02/index.php
@@ -0,0 +1,17 @@
+<?php
+	include_once "$_SERVER[DOCUMENT_ROOT]/workspace/Estudos/head.php";
+	
+	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/functions/html.php';
+	
+	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/menu.php';
+
+	
+	if (isset($_POST['enviar'])){
+		calculaDobro();
+	}else{
+		gerarInputNumero(30);
+	}
+	
+	
+
+?>
