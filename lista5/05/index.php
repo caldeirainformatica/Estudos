a4c330f73f9a2541bb558fe5374119ff8501ecd2
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-09-30 17:59:26 -0300

    paginas não implementadas

---
+++ lista5/05/index.php
@@ -0,0 +1,13 @@
+<html>
+<?php 
+	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/head.php';
+
+	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/menu.php';
+?>
+
+	<body>
+		<div class="container" style="margin-top: 60px">	
+			<font color="red"><h1 align="center">PAGINA NÃO IMPLEMENTADA</h1></font>
+		</div>
+	</body>
+</html>
