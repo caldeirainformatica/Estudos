f9222aff771697b57ff63c14b22a8271b2d15ff3
Ricardo Caldeira <ricardo@cpdsi.com.br>
2015-10-03 10:15:31 -0300

    Termino do prazo de entrega

--- lista5/09/index.php
+++ lista5/09/index.php
@@ -1,13 +1,55 @@
 <html>
-<?php 
-	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/head.php';
-
-	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/menu.php';
-?>
-
-	<body>
-		<div class="container" style="margin-top: 60px">	
-			<font color="red"><h1 align="center">PAGINA NÃO IMPLEMENTADA</h1></font>
-		</div>
-	</body>
+	<?php
+		include_once "$_SERVER[DOCUMENT_ROOT]/workspace/Estudos/head.php";
+		
+		include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/functions/html.php';
+		
+		include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/menu.php';
+		
+		?>
+		<body>
+			<div class="container" style="margin-top: 60px">
+				<form action="resultado.php">
+		<?php 
+				for ($cont = 1; $cont <= 200; $cont++){
+					echo '<fieldset>';
+						echo '<legend>Pesquisa de qualidade '.$cont.'</legend>';
+						echo '<label for="nome_'.$cont.'">Qual nome do aluno '.$cont.'</label><br/>';
+						echo '<input type="text" id="nome_'.$cont.'" name="nome_'.$cont.'"><br/>';
+						echo '<label for="idade_'.$cont.'">Qual a idade do aluno '.$cont.'</label> <br/>';
+						echo '<input type="number" id="idade_'.$cont.'" name="idade_'.$cont.'"><br/>';
+						echo '<br/>';
+						echo '<label>Qual a nota para a peça sendo 0 muito ruim e 10 muito bom</label>';
+						echo '<label for="nota_'.$cont.'"> 0 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" checked="checked" value="0">';
+						echo '<label for="nota_'.$cont.'"> 1 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="1">';
+						echo '<label for="nota_'.$cont.'"> 2 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="2">';
+						echo '<label for="nota_'.$cont.'"> 3 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="3">';
+						echo '<label for="nota_'.$cont.'"> 4 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="4">';
+						echo '<label for="nota_'.$cont.'"> 5 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="5">';
+						echo '<label for="nota_'.$cont.'"> 6 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="6">';
+						echo '<label for="nota_'.$cont.'"> 7 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="7">';
+						echo '<label for="nota_'.$cont.'"> 8 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="8">';
+						echo '<label for="nota_'.$cont.'"> 9 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="9">';
+						echo '<label for="nota_'.$cont.'"> 10 </label>';
+						echo '<input type="radio" name="nota_'.$cont.'" id="nota_'.$cont.'" value="10"><br/><br/><br/>';
+					echo '</fieldset>';
+					
+						
+				}
+		?>			<input type="submit" name="enviar" id="enviar" value="Enviar">
+					<input type="reset" name="limpar" id="limpar" value="Limpar">
+				
+				</form>
+			</div>
+		</body>	
 </html>
--- lista5/09/resultado.php
+++ lista5/09/resultado.php
@@ -1,1 +1,73 @@
-<?php
+<html>
+	<?php 
+	include_once "$_SERVER[DOCUMENT_ROOT]/workspace/Estudos/head.php";
+	
+ 	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/functions/html.php';
+	
+ 	include_once $_SERVER['DOCUMENT_ROOT'].'/workspace/Estudos/menu.php';
+	
+	$sistemaInformacao = 0;
+	$cienciaComputacao = 0;
+	$engenhariaCivil = 0;
+	$idadeSistemaInformacao = 0;
+	$idadecienciaComputacao = 0;
+	$idadeEngenhariaCivil = 0;
+	$somaIdade1 = 0;
+	$somaIdade3 = 0;
+	$somaIdade2 = 0;
+	$mediaIdade1 = 0;
+	$mediaIdade2 = 0;
+	$mediaIdade3 = 0;
+	
+		for ($cont = 1; $cont <= 500; $cont++){
+			$curso = $_GET['cursoAluno_'.$cont];
+			$idade = $_GET['idade_'.$cont];
+			if ($curso == 1){
+				$sistemaInformacao = $sistemaInformacao + 1;
+				$somaIdade1 = $idade + $somaIdade1;
+			}elseif ($curso == 2){
+				$cienciaComputacao = $cienciaComputacao + 1;
+				$somaIdade2 = $idade + $somaIdade2;
+				echo $somaIdade2.'<br>';
+				echo $cienciaComputacao.'<br>';
+				
+			}else {
+				$engenhariaCivil = $engenhariaCivil + 1;
+				$somaIdade3 = $idade + $somaIdade3;
+				
+			}
+			
+			if (($idade >19) && ($idade < 26)){
+				if ($curso == 1){
+					$idadeSistemaInformacao = $idadeSistemaInformacao + 1;
+				}
+				if ($curso == 2){
+					$idadecienciaComputacao = $idadecienciaComputacao +1;
+				}
+				if ($curso == 3){
+					$idadeEngenhariaCivil = $idadeEngenhariaCivil +1;
+				}
+			}
+			
+		}
+		$mediaIdade1 = ($somaIdade1 / $sistemaInformacao);
+		$mediaIdade2 = ($somaIdade2 / $cienciaComputacao);		
+		$mediaIdade3 = ($somaIdade3 / $engenhariaCivil);		
+		
+	
+		
+		echo '<div class="container" style="margin-top: 60px">';
+			echo 'No curso de Sistema de Informação tem ' . $sistemaInformacao . ' alunos. Dentre eles '. $idadeSistemaInformacao . ' tem idade entre 20 e 25 anos<br/>';
+			echo 'No curso de Ciência da Computação tem ' . $cienciaComputacao . ' alunos. Dentre eles '. $idadecienciaComputacao . ' tem idade entre 20 e 25 anos<br/>';
+			echo 'No curso de Engenharia Civil tem ' . $engenhariaCivil . ' alunos. Dentre eles '. $idadeEngenhariaCivil . ' tem idade entre 20 e 25 anos<br/>';
+			if (($mediaIdade1 < $mediaIdade2) && ($mediaIdade1 < $mediaIdade3)){
+				echo 'O curso com a menor média de idade é Sistema de Informção';
+			}elseif (($mediaIdade2 < $mediaIdade1) && ($mediaIdade2 < $mediaIdade3)){
+				echo 'O curso com a menor média de idade é Ciência da Computação';
+			}else{
+				echo 'O curso com a menor média de idade é Engenharia Civil';
+			}
+		echo '</div>';
+		
+	?>
+</html>
